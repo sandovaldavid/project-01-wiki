cb27fd65dd072d002c3d365a6928466942562a57
sandovaldavid <sandovaldavid2201@gmail.com>
2024-09-03 15:56:36 -0500

    :tada: initial project #2

--- wiki/wsgi.py
+++ wiki/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for project1_wiki project.
+WSGI config for wiki project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project1_wiki.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki.settings')
 
 application = get_wsgi_application()
