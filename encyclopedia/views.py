3ba0acf94df21766b7477ada82e8b0a4da1ef92b
sandovaldavid <sandovaldavid2201@gmail.com>
2024-09-04 15:31:17 -0500

    :sparkles: Implement Entry Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from . import util
@@ -7,4 +8,12 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def entry(request, title):
+    content = util.get_entry(title)
+    if content is None:
+        return render(request, "encyclopedia/404.html")
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title.upper(),
+            "content": content
+        })
