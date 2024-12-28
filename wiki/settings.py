8ebb7962016f1f9a9a9ca474faa158cccb8257b7
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-27 21:40:24 -0500

    🔧 Update: refactor BASE_DIR path definition and add template directory

--- wiki/settings.py
+++ wiki/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'wiki.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'encyclopedia/templates',    
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
